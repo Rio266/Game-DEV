337af7789a611abc97817387e771f520aaa21f3e
Rio266 <aksharalok12@gmail.com>
2026-03-22 11:51:58 +0000

    Add files via upload

--- Dic.py
+++ Dic.py
@@ -26,3 +26,35 @@ print(sample_dictionary)
 #2. Using pop() method
 sample_dictionary.pop("Favourite Food")
 print(sample_dictionary)
+
+#Print all keys
+print(sample_dictionary.keys())
+
+#Print all values
+print(sample_dictionary.values())
+
+#Add list into dictionary
+sample_dictionary["marks"] = [3, 6, 9, 12]
+print(sample_dictionary)
+
+#Access value in list in dictionary
+print(sample_dictionary["marks"][2])
+
+#Create nested dictionary
+dictionary_2 = {
+    "Aarav" : {
+        "age" : 12,
+        "hobby" : "cycling"
+    },
+    "Rohan" : {
+        "age" : 15,
+        "hobby" : "football"
+    }
+}
+
+#Print all the keys and values
+print(dictionary_2.values())
+print(dictionary_2.keys())
+
+#Access value in nested dictionary
+print(dictionary_2["Rohan"]["age"])
