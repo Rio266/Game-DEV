f1297bf1fa19227c8f37a9a2d013387797f88c74
Rio266 <aksharalok12@gmail.com>
2026-05-03 11:04:50 +0100

    Add files via upload

---
+++ aliengamehw.py
@@ -0,0 +1,29 @@
+import pgzrun, random
+WIDTH = 600
+HEIGHT = 600
+image = Actor("alienhw")
+message = ""
+def draw():
+    screen.fill("black")
+    image.draw()
+    screen.draw.text(message, (300, 100), fontsize = 20)
+def update():
+    if keyboard.left:
+        image.x -= 5
+    if keyboard.right:
+        image.x += 5
+    if keyboard.up:
+        image.y -= 5
+    if keyboard.down:
+        image.y += 5
+def move():
+    image.x = random.randint(70, 530)
+    image.y = random.randint(100, 500)
+def on_mouse_down(pos):
+    global message
+    if image.collidepoint(pos):
+        move()
+        message = "Alien Hit"
+    else:
+        message = "Alien Missed"
+pgzrun.go()
