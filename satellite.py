42e537602c8ca29b1e30577653b458d8eff97ad9
Rio266 <aksharalok12@gmail.com>
2026-05-04 05:49:20 +0100

    Add files via upload

--- satellite.py
+++ satellite.py
@@ -1,4 +1,4 @@
-import pgzrun, random
+import pgzrun, random, time
 WIDTH = 600
 HEIGHT = 480
 
@@ -13,11 +13,14 @@ total_time = 0
 total_satellites = 8
 
 def create():
+    global start_time
     for i in range(total_satellites):
         satellite_character = Actor("satellitecharacter")
         satellite_character.pos = (random.randint(40, 560), random.randint(40, 440))
         satellites.append(satellite_character)
+    start_time = time.time()
 def draw():
+    global total_time
     screen.blit("satellitebackground", (0, 0))
     n = 1
     for i in satellites:
@@ -26,6 +29,11 @@ def draw():
         n += 1
     for i in lines:
         screen.draw.line(i[0], i[1], "orange")
+    if next_satellite < total_satellites:
+        total_time = time.time() - start_time
+        screen.draw.text(str(round(total_time, 1)), (20, 20), color = "blue", fontsize = 36)
+    else:
+        screen.draw.text(str(round(total_time, 1)), (20, 20), color = "blue", fontsize = 36)
 def update():
     pass
 def on_mouse_down(pos):
@@ -34,6 +42,7 @@ def on_mouse_down(pos):
         if satellites[next_satellite].collidepoint(pos):
             if next_satellite:
                 lines.append((satellites[next_satellite - 1].pos, satellites[next_satellite].pos))
+            next_satellite += 1
         else:
             lines = []
             next_satellite = 0
